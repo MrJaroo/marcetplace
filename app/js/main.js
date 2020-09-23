f508943e0970a36ec842377d96377c5eb17fa854
MrJaroo <mishadolininma@mail.ru>
2020-09-23 09:16:46 +0400

    add product and feedback

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    var mixer = mixitup('.product__item');
+    
+    $('.rate-starr').rateYo({
+        rating: 4.9,
+        starWidth:"13px",
+        ratedFill:"#ffc000",
+    });
+});
